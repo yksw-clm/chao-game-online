06f7dfab91a7abf7064f3944240a6aa5ef0ca248
yksw-clm <tkm.nyoro1218@gmail.com>
2025-07-05 16:21:28 +0900

    add: socket.io接続を管理するzustandストアを実装

--- packages/client/src/vite-env.d.ts
+++ packages/client/src/vite-env.d.ts
@@ -7,6 +7,8 @@ interface ImportMetaEnv {
 	readonly VITE_APP_FIREBASE_STORAGE_BUCKET: string;
 	readonly VITE_APP_FIREBASE_MESSAGING_SENDER_ID: string;
 	readonly VITE_APP_FIREBASE_APP_ID: string;
+
+	readonly VITE_APP_SOCKET_URL: string;
 }
 
 interface ImportMeta {
