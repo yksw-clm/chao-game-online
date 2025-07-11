876880c265ce26d2a00ad1636496f995601d55d1
yksw-clm <tkm.nyoro1218@gmail.com>
2025-07-12 02:25:18 +0900

    update: ビルドエラーを修正

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./global.css";
-import { TEST } from "@chao-game-online/shared";
 
-createRoot(document.getElementById("root")!).render(<StrictMode>{TEST}</StrictMode>);
+createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
